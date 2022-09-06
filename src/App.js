87c7086cb2f67d7eefc8fcf31ebf949e605adb6c
Shamaem Saqib <shamaem_saqib@hotmail.com>
2022-09-07 04:39:45 +0500

    Updated the App class component to functional componenet with Hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { onSnapshot } from "firebase/firestore";
-import { connect } from "react-redux/es/exports";
-import { createStructuredSelector } from "reselect";
+import { useDispatch, useSelector } from "react-redux/es/exports";
 
 import "./App.css";
 
@@ -18,70 +17,86 @@ import { addProfileDocumentToFirestore, auth } from "./firebase/firebase.utils";
 
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selectors";
+import { useEffect } from "react";
 
-class App extends React.Component {
-  unsubscribeFromAuth = null;
+const App = () => {
+  const dispatch = useDispatch();
+  const currentUser = useSelector(selectCurrentUser);
 
-  componentDidMount() {
-    const { setCurrentUser } = this.props;
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
+  useEffect(() => {
+    const unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
       if (user) {
         const userDocRef = await addProfileDocumentToFirestore(user);
 
         onSnapshot(userDocRef, (snapShot) => {
-          setCurrentUser({
-            ...snapShot.data(),
-          });
+          dispatch(
+            setCurrentUser({
+              ...snapShot.data(),
+            })
+          );
         });
       } else {
-        setCurrentUser(user);
+        dispatch(setCurrentUser(user));
       }
     });
-    // addShopToFireStore("sections", sectionsForPreview);
-  }
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
+    return () => unsubscribeFromAuth();
+  }, [dispatch]);
 
-  render() {
-    return (
-      <div className="app">
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<SectionsList />} />
-          <Route exact path="/shop" element={<Shop />}>
-            <Route
-              exact
-              path="/shop"
-              element={<SectionPreviewWrapperContainer />}
-            />
-            <Route
-              exact
-              path="/shop/:sectionID"
-              element={<SectionContainer />}
-            />
-          </Route>
-          <Route exact path="/checkout" element={<Checkout />} />
+  // componentDidMount() {
+  //   const { setCurrentUser } = this.props;
+  //   this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
+  //     if (user) {
+  //       const userDocRef = await addProfileDocumentToFirestore(user);
+
+  //       onSnapshot(userDocRef, (snapShot) => {
+  //         setCurrentUser({
+  //           ...snapShot.data(),
+  //         });
+  //       });
+  //     } else {
+  //       setCurrentUser(user);
+  //     }
+  //   });
+  //   // addShopToFireStore("sections", sectionsForPreview);
+  // }
+
+  // componentWillUnmount() {
+  //   this.unsubscribeFromAuth();
+  // }
+
+  return (
+    <div className="app">
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<SectionsList />} />
+        <Route exact path="/shop" element={<Shop />}>
           <Route
             exact
-            path="/sign-in"
-            element={
-              this.props.currentUser ? <Navigate to="/" /> : <Register />
-            }
+            path="/shop"
+            element={<SectionPreviewWrapperContainer />}
           />
-        </Routes>
-      </div>
-    );
-  }
-}
+          <Route exact path="/shop/:sectionID" element={<SectionContainer />} />
+        </Route>
+        <Route exact path="/checkout" element={<Checkout />} />
+        <Route
+          exact
+          path="/sign-in"
+          element={currentUser ? <Navigate to="/" /> : <Register />}
+        />
+      </Routes>
+    </div>
+  );
+};
 
-const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-});
+// const mapStateToProps = createStructuredSelector({
+//   currentUser: selectCurrentUser,
+// });
 
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-});
+// const mapDispatchToProps = (dispatch) => ({
+//   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+// });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default App;
