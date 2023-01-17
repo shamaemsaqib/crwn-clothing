eb131f091e10b73e4d04ff0bd7787ed7dfc6a26b
Shamaem Saqib <shamaem_saqib@hotmail.com>
2023-01-18 03:07:56 +0500

    Performance optimization: used lazy imports in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, lazy, Suspense, useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { onSnapshot } from "firebase/firestore";
 import { useDispatch, useSelector } from "react-redux/es/exports";
@@ -7,12 +7,6 @@ import "./App.css";
 
 import Header from "./components/header/header.component";
 import Home from "./routes/home/home.component";
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/checkout/checkout.component";
-import SectionContainer from "./routes/section/section.container";
-import SectionPreviewWrapperContainer from "./routes/section-preview-wrapper/section-preview-wrapper.container";
-import PaymentSuccessful from "./routes/payment-successful/payment-succussful.component";
-import PaymentUnsuccessful from "./routes/payment-unsuccessful/payment-unsuccussful.component";
 
 import {
   addProfileDocumentToFirestore,
@@ -21,8 +15,24 @@ import {
 
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selectors";
-import { useEffect } from "react";
-import Auth from "./routes/auth/auth.component";
+import DottedSpinner from "./components/withSpinnerHOC/dotted-spinner/dotted-spinner.component";
+
+//Lazy imports
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
+const SectionContainer = lazy(() =>
+  import("./routes/section/section.container")
+);
+const SectionPreviewWrapperContainer = lazy(() =>
+  import("./routes/section-preview-wrapper/section-preview-wrapper.container")
+);
+const PaymentSuccessful = lazy(() =>
+  import("./routes/payment-successful/payment-succussful.component")
+);
+const PaymentUnsuccessful = lazy(() =>
+  import("./routes/payment-unsuccessful/payment-unsuccussful.component")
+);
+const Auth = lazy(() => import("./routes/auth/auth.component"));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -71,36 +81,42 @@ const App = () => {
   // }
 
   return (
-    <div className="app">
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/shop" element={<Shop />}>
+    <Suspense fallback={DottedSpinner}>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/shop" element={<Shop />}>
+            <Route
+              exact
+              path="/shop"
+              element={<SectionPreviewWrapperContainer />}
+            />
+            <Route
+              exact
+              path="/shop/:sectionID"
+              element={<SectionContainer />}
+            />
+          </Route>
+          <Route exact path="/checkout" element={<Checkout />} />
           <Route
             exact
-            path="/shop"
-            element={<SectionPreviewWrapperContainer />}
+            path="/auth"
+            element={currentUser ? <Navigate to="/" /> : <Auth />}
           />
-          <Route exact path="/shop/:sectionID" element={<SectionContainer />} />
-        </Route>
-        <Route exact path="/checkout" element={<Checkout />} />
-        <Route
-          exact
-          path="/auth"
-          element={currentUser ? <Navigate to="/" /> : <Auth />}
-        />
-        <Route
-          exact
-          path="/payment-successful"
-          element={<PaymentSuccessful />}
-        />
-        <Route
-          exact
-          path="/payment-unsuccessful"
-          element={<PaymentUnsuccessful />}
-        />
-      </Routes>
-    </div>
+          <Route
+            exact
+            path="/payment-successful"
+            element={<PaymentSuccessful />}
+          />
+          <Route
+            exact
+            path="/payment-unsuccessful"
+            element={<PaymentUnsuccessful />}
+          />
+        </Routes>
+      </div>
+    </Suspense>
   );
 };
 
